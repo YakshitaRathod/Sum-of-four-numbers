e69a90a1c57cca02f35b7725993011dd4aebe98a
chavitodionisio <61675850+chavitodionisio@users.noreply.github.com>
2020-03-05 16:22:00 +0300

    Add files via upload

---
+++ SUM OF FOUR NUMBERS.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter First Number:\n"))
+num2 = int(input("Enter second Number:\n"))
+num3 = int(input("Enter third Number:\n"))
+num4 = int(input("Enter forth Number:\n"))
+answer = num1 + num2 + num3 + num4
+print()
+print("The value of the four numbers you entered:", answer)
+
